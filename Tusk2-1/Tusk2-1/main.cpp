6aeb103a86d29b065e83ea39365aaad7968c2ea0
None <125607064+ExpertRanger@users.noreply.github.com>
2023-02-26 22:07:37 +0300

    Tusk2-1
    Tusk2-1

---
+++ Tusk2-1/Tusk2-1/main.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <cmath>
+#include "Triangle.h"
+using namespace std;
+
+
+int main()
+{
+    Triangle mas[3];
+    double a, b, c;
+    for (int i = 0; i < 3; i++) {
+        cout << "Triangle № " << i + 1 << "\na,b,c: " << endl;
+        cin >> a >> b >> c;
+        mas[i].set(a, b, c);
+        if (!(mas[i].exst_tr())) {
+            mas[i].show();
+            cout << "NEXT.... " << endl;
+            i--;
+        }
+    }
+    for (int i = 0; i < 3; i++) {
+        mas[i].show();
+        setlocale(LC_ALL, "");
+        cout << "P = " << mas[i].perimetr() << endl;
+        cout << "S = " << mas[i].square() << endl;
+    }
+    return 0;
+}
